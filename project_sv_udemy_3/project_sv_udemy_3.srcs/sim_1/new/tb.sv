a81c057c83a2dbc36c2361837ae7a948aab8099e
adityacpn <adityachopan@gmail.com>
2022-08-24 09:50:45 +0530

    updated auto

---
+++ project_sv_udemy_3/project_sv_udemy_3.srcs/sim_1/new/tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// 
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/18/2022 08:08:27 PM
+// Design Name: 
+// Module Name: tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb();
+    
+endmodule
